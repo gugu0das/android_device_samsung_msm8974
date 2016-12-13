7ddc23369b4052d301d1e9bdf88b4242db7f5ef1
gugu0das <gugu0das@gmail.com>
2016-12-13 20:01:34 +0900

    CyanogenMod 13.0 jactivelteskt Device Source

---
+++ cm.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, device/samsung/jactivelteskt/full_jactivelteskt.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := jactivelteskt
+PRODUCT_NAME := cm_jactivelteskt
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jactivelteskt \
+    PRODUCT_DEVICE=jactivelteskt \
+    TARGET_DEVICE=jactivelteskt \
+    PRODUCT_MODEL=SHV-E470S \
+    BUILD_FINGERPRINT=samsung/jactivelteskt/jactivelteskt:5.0.1/LRX22C/E470SKSUCOH1:user/release-keys \
+    PRIVATE_BUILD_DESC="jactivelteskt-user 5.0.1 LRX22C E470SKSUCOH1 release-keys"
